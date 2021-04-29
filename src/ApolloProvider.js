4a7daa2df7cf1196861bbd4cc2fb40d2e3909f69
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-04-29 10:21:07 +0100

    fixed apollo boilerplate

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,9 +1,6 @@
-import React from "react";
-import App from "./App";
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
 import { ENDPOINT } from "./config";
 
 // Apollo boilerplate
@@ -16,8 +13,4 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-export default (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import ApolloProvider from "./ApolloProvider";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./ApolloProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
