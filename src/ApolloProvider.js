ab08569e8de6a0fea08cdcf0113e2963f3301f09
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-04-29 10:07:54 +0100

    apollo boilerplate

---
+++ src/ApolloProvider.js
@@ -0,0 +1,23 @@
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ENDPOINT } from "./config";
+
+// Apollo boilerplate
+const httpLink = createHttpLink({
+  uri: ENDPOINT,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
